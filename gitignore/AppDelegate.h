6217acb7a4e2fd7bb5c9c9447be2998152eb84eb
xxp <931968649@qq.com>
2018-07-12 20:33:23 +0800

    Initial Commit

---
+++ gitignore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitignore
+//
+//  Created by xy_yanfa_imac on 2018/7/12.
+//  Copyright © 2018年 xy_yanfa_imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
